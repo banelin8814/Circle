da6b70998bfddc277efb107c479edb7fae61cde7
Lin <banelin30@gmail.com>
2023-10-18 17:09:40 +0800

    Initial Commit

---
+++ goSocial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  goSocial
+//
+//  Created by 林佑淳 on 2023/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
