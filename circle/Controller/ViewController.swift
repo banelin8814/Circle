fe6d6adaa1985e11a35cf73ceef01205c723f13d
Lin <banelin30@gmail.com>
2023-10-22 10:21:51 +0800

    10/22 corelocation 設定好，試圖解決黑色頻幕的問題

--- circle/Controller/ViewController.swift
+++ circle/Controller/ViewController.swift
@@ -7,142 +7,83 @@
 
 
 import UIKit
-import AVFoundation//B.有關拍照
 import CoreLocation//A.有關定位
+import AVFoundation//B.有關拍照
 
 
 class ViewController: UIViewController {
     
-    let loctionManager = CLLocationManager() //A.有關定位
-   
     
-    @IBOutlet weak var showImage: UIImageView!
     
-    //captute session
-    var session: AVCaptureSession?//B.有關拍照
+    let locationManager = CLLocationManager() //A.有關定位
     
-    //photo output
-    var output =  AVCapturePhotoOutput()//B.有關拍照
-     
-    //video preview
-    let previewLayer = AVCaptureVideoPreviewLayer()//B.有關拍照
     
-    //ShutterButton 閃光燈
-    private let shutterButton: UIButton = {//B.有關拍照
-        let button = UIButton(frame: CGRect(x:0,y:0, width: 100, height: 100))
-        button.layer.cornerRadius = 100
-        button.layer.borderWidth = 10
-        button.layer.borderColor = UIColor.white.cgColor
-        return button
-    }()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loctionManager.requestWhenInUseAuthorization()//A.有關定位/要求存取
-
-        view.backgroundColor = .white
-        view.layer.addSublayer(previewLayer)
-        view.addSubview(shutterButton)
-        checkCameraPermissions()
         
-        shutterButton.addTarget(self, action: #selector(didTapTakePhoto), for: .touchUpInside)
-            
-        }
+        locationManager.delegate = self//A.有關定位
+        locationManager.requestAlwaysAuthorization()//A.有關定位/要求存取
+        // 監聽位置更新
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest//A.有關定位提供最準確的位置信息
+        locationManager.startUpdatingLocation()//A.有關定位/一直在更新位置
         
-    //B.有關拍照
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        previewLayer.frame = view.bounds
         
-        shutterButton.center = CGPoint(x: view.frame.size.width / 2, y: view.frame.size.height - 100)
-     }
-    //B.有關拍照
-        private func checkCameraPermissions() {  //確保我們有權限
-            switch AVCaptureDevice.authorizationStatus(for: .video) {
-                
-            case .notDetermined:
-                //要求權限
-                AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
-                    guard granted else {
-                        return
-                    }
-                    DispatchQueue.main.async {
-                        self?.setUpCamera()
-                        
-                    }
-                }
-            case .restricted:
-                break
-            case .denied:
-                break
-            case .authorized:
-                setUpCamera()
-            @unknown default:
-                break
-            }
-        }
-    
-     
-    //B.有關拍照
-        private func setUpCamera() {
-            let session = AVCaptureSession()
-            if let device = AVCaptureDevice.default(for: .video){
-                do{
-                   let input = try AVCaptureDeviceInput(device: device)
-                    if session.canAddInput(input){
-                        session.addInput(input)
-                    }
-                    
-                    if session.canAddOutput(output) {
-                        session.addOutput(output)
-                    }
-                    
-                    previewLayer.videoGravity = .resizeAspectFill
-                    previewLayer.session = session  //add session to the preview layer
-                    
-                    session.startRunning()
-                    self.session = session //retain session into global全局
-                    
-                    
-                 }
-                catch{
-                    print(error)
-                    
-                    
-                }
-            }
         
-            
-            
+        // Auto layout, variables, and unit scale are not yet supported
+        let view = UIView()
+        view.frame = CGRect(x: 0, y: 0, width: 390, height: 844)
+        view.layer.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+        
+        // Auto layout, variables, and unit scale are not yet supported
+        let view2 = UIView()
+        view2.frame = CGRect(x: 0, y: 0, width: 367, height: 655)
+        let parent = self.view!
+        parent.addSubview(view)
+        view2.translatesAutoresizingMaskIntoConstraints = false
+        view2.widthAnchor.constraint(equalToConstant: 367).isActive = true
+        view2.heightAnchor.constraint(equalToConstant: 655).isActive = true
+        view2.leadingAnchor.constraint(equalTo: parent.leadingAnchor, constant: 12).isActive = true
+        view2.topAnchor.constraint(equalTo: parent.topAnchor, constant: 137).isActive = true
+        
     }
-    
-    
-    @objc private func didTapTakePhoto(){
-        output.capturePhoto(with: AVCapturePhotoSettings(), 
-                            delegate: self)
-    }
-    
 }
-//B.有關拍照
-extension ViewController: AVCapturePhotoCaptureDelegate {
-    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-        guard let data = photo.fileDataRepresentation() else {
-            return
-        }
-        let image = UIImage(data: data)
-        
-        session?.stopRunning()
-        
-        
-        let imageView = UIImageView(image: image)
-        imageView.contentMode = .scaleAspectFill
-        imageView.frame = view.bounds
-        view.addSubview(imageView)
-    }
+
+
+
+
+
+
+
+
+//MARK: - CLLocationManagerDelegate
+
+
+A.有關定位
+extension ViewController: CLLocationManagerDelegate {
     
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+           
+        // 獲取使用者 A 的最新位置
+           let locationA = locations.first!
+
+           // 獲取使用者 B 的最新位置
+           let locationB = locations.last!
+
+           // 判斷兩位使用者是否在同一個空間下
+           let distance = locationA.distance(from: locationB)
+           if distance < 10 {
+               // 兩位使用者在同一個空間下
+               // ...
+           }
+       }
 }
-//做一些更改
 
+//MARK: - AVCapturePhotoCaptureDelegate
 
- 
+//extension ViewController: AVCapturePhotoCaptureDelegate {
+//
+//    
+//}
